df2c1fcc86d0a1142008269ac5561b6c3bd5a47a
Taiye Ajimati <taiyeajimati@gmail.com>
2022-10-05 17:55:53 +0400

    last office commit

---
+++ manage-landing-page/src/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div >
+        <h1 className='font-[400]'>hello</h1>
+    </div>
+  )
+}
--- manage-landing-page/tailwind.config.js
+++ manage-landing-page/tailwind.config.js
@@ -4,6 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      BrightRed: 	'#f25f3a',
+      DarkBlue: '	#242d52',
+      DarkGrayishBlue: '	#9095a7',
+      VeryDarkBlue: '#1d1e25',
+      VeryPaleRed: '	#ffefeb',
+      VaryLightGray: '#fafafa'
+    },
+
+    container:{
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },      
+    },
+
+    fontFamily: {
+      'VietnamPro': ['Be Vietnam Pro', 'system-ui']
+    },
+    fontWeight: {
+      normal: 400,
+      bold: 500,
+      semibold: 700,
+    },
     extend: {},
   },
   plugins: [],
